9a2c2e364ec30ce93a88ea25ab15533c19952afe
AdaevIlyas <il_adaev@mail.ru>
2026-01-19 17:42:56 +0600

    add assembly rn

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-primary flex-1 justify-center items-center">
+      <Text className="text-4xl text-[#ff0] font-bold">Welcome!</Text>
+    </View>
+  );
+}
